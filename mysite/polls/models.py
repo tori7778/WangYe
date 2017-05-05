f29e1086564656da31d979120564b93de042d50c
tori7778 <jill04010401@gmail.com>
2017-05-05 22:32:04 +0800

    django

---
+++ mysite/polls/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    name_text=models.CharField(max_length=200)
+    studentid_text=models.CharField(max_length=200)
+    def __str__(self):
+        return self.question_text
+
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
